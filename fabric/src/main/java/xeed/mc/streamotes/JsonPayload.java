437675fec77facb89c8136b3459dcd46289f0245
Xspeed <xspeedpl@windowslive.com>
2024-06-14 16:17:29 +0200

    Resolved Paper to Fabric packet problems

--- fabric/src/main/java/xeed/mc/streamotes/JsonPayload.java
+++ fabric/src/main/java/xeed/mc/streamotes/JsonPayload.java
@@ -4,15 +4,17 @@ import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.packet.CustomPayload;
 
+import java.nio.charset.StandardCharsets;
+
 record JsonPayload(String json) implements CustomPayload {
 	public static final CustomPayload.Id<JsonPayload> PACKET_ID = new CustomPayload.Id<>(StreamotesCommon.IDENT);
 	public static final PacketCodec<PacketByteBuf, JsonPayload> PACKET_CODEC = new PacketCodec<>() {
 		public JsonPayload decode(PacketByteBuf byteBuf) {
-			return new JsonPayload(byteBuf.readString());
+			return new JsonPayload(byteBuf.readCharSequence(byteBuf.readableBytes(), StandardCharsets.UTF_8).toString());
 		}
 
 		public void encode(PacketByteBuf byteBuf, JsonPayload payload) {
-			byteBuf.writeString(payload.json());
+			byteBuf.writeCharSequence(payload.json(), StandardCharsets.UTF_8);
 		}
 	};
 
--- paper/src/main/java/xeed/mc/paper/streamotes/Streamotes.java
+++ paper/src/main/java/xeed/mc/paper/streamotes/Streamotes.java
@@ -15,6 +15,7 @@ import org.jetbrains.annotations.Nullable;
 import org.spigotmc.event.player.PlayerSpawnLocationEvent;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Objects;
 import java.util.logging.Level;
@@ -22,107 +23,108 @@ import java.util.regex.Pattern;
 
 @SuppressWarnings("unused")
 public final class Streamotes extends JavaPlugin implements Listener {
-    public static final Pattern VALID_CHANNEL_PATTERN = Pattern.compile("[_a-zA-Z]\\w+");
+	public static final Pattern VALID_CHANNEL_PATTERN = Pattern.compile("[_a-zA-Z]\\w+");
 
-    private static final String CHANNEL = "xeed:streamotes";
-    private static final String COMMAND = "streamotes";
-    private static final String RELOAD = "reload";
+	private static final String CHANNEL = "xeed:streamotes";
+	private static final String COMMAND = "streamotes";
+	private static final String RELOAD = "reload";
 
-    private ModConfigModel config = new ModConfigModel();
+	private ModConfigModel config = new ModConfigModel();
 
-    private String getConfigPath() {
-        return getServer().getPluginsFolder().toPath().resolve("../config/streamotes.json5").toString();
-    }
+	private String getConfigPath() {
+		return getServer().getPluginsFolder().toPath().resolve("../config/streamotes.json5").toString();
+	}
 
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onPlayerSpawn(PlayerSpawnLocationEvent event) {
-        getServer().getScheduler().runTaskLaterAsynchronously(this, () -> {
-            event.getPlayer().sendPluginMessage(this, CHANNEL, createConfigPacket());
-            getLogger().info("Sent config packet to " + event.getPlayer().getName());
-        }, 5);
-    }
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void onPlayerSpawn(PlayerSpawnLocationEvent event) {
+		getServer().getScheduler().runTaskLaterAsynchronously(this, () -> {
+			event.getPlayer().sendPluginMessage(this, CHANNEL, createConfigPacket());
+			getLogger().info("Sent config packet to " + event.getPlayer().getName());
+		}, 5);
+	}
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onReload(ServerResourcesReloadedEvent event) {
-        reloadModConfig();
-        getServer().sendPluginMessage(this, CHANNEL, createConfigPacket());
-    }
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onReload(ServerResourcesReloadedEvent event) {
+		reloadModConfig();
+		getServer().sendPluginMessage(this, CHANNEL, createConfigPacket());
+	}
 
-    private void reloadModConfig() {
-        var gson = new Gson();
-        try (var input = new InputStreamReader(new FileInputStream(getConfigPath()))) {
-            config = gson.fromJson(input, ModConfigModel.class);
-            ModConfigModel.verifyChannels(config);
-            getLogger().info("Config reloaded");
-        } catch (IOException e) {
-            getLogger().log(Level.SEVERE, "Config reload failed", e);
-        }
-    }
+	private void reloadModConfig() {
+		var gson = new Gson();
+		try (var input = new InputStreamReader(new FileInputStream(getConfigPath()))) {
+			config = gson.fromJson(input, ModConfigModel.class);
+			ModConfigModel.verifyChannels(config);
+			getLogger().info("Config reloaded");
+		}
+		catch (IOException e) {
+			getLogger().log(Level.SEVERE, "Config reload failed", e);
+		}
+	}
 
-    private void saveModConfig() {
-        var gson = new GsonBuilder().setPrettyPrinting().create();
-        try (var output = new OutputStreamWriter(new FileOutputStream(getConfigPath()))) {
-            ModConfigModel.verifyChannels(config);
-            output.write(gson.toJson(config));
-        } catch (IOException e) {
-            getLogger().log(Level.SEVERE, "Config save failed", e);
-        }
-    }
+	private void saveModConfig() {
+		var gson = new GsonBuilder().setPrettyPrinting().create();
+		try (var output = new OutputStreamWriter(new FileOutputStream(getConfigPath()))) {
+			ModConfigModel.verifyChannels(config);
+			output.write(gson.toJson(config));
+		}
+		catch (IOException e) {
+			getLogger().log(Level.SEVERE, "Config save failed", e);
+		}
+	}
 
-    @Override
-    public void onEnable() {
-        if (!new File(getConfigPath()).exists()) {
-            saveModConfig();
-        }
+	@Override
+	public void onEnable() {
+		if (!new File(getConfigPath()).exists()) {
+			saveModConfig();
+		}
 
-        var cmd = Objects.requireNonNull(getCommand(COMMAND));
-        cmd.setUsage("Usage: /streamotes [reload]");
-        cmd.setTabCompleter(this);
-        cmd.setExecutor(this);
+		var cmd = Objects.requireNonNull(getCommand(COMMAND));
+		cmd.setUsage("Usage: /streamotes [reload]");
+		cmd.setTabCompleter(this);
+		cmd.setExecutor(this);
 
-        getServer().getPluginManager().registerEvents(this, this);
-        getServer().getMessenger().registerOutgoingPluginChannel(this, CHANNEL);
-        reloadModConfig();
-    }
+		getServer().getPluginManager().registerEvents(this, this);
+		getServer().getMessenger().registerOutgoingPluginChannel(this, CHANNEL);
+		reloadModConfig();
+	}
 
-    @Override
-    public void onDisable() {
-        getServer().getMessenger().unregisterOutgoingPluginChannel(this, CHANNEL);
-    }
+	@Override
+	public void onDisable() {
+		getServer().getMessenger().unregisterOutgoingPluginChannel(this, CHANNEL);
+	}
 
-    private byte[] createConfigPacket() {
-        var buf = Unpooled.buffer();
-        var gson = new Gson();
-        PacketUtils.writeString(buf, gson.toJson(config, ModConfigModel.class), 32767);
-        return buf.array();
-    }
+	private byte[] createConfigPacket() {
+		var buf = Unpooled.buffer();
+		var json = new Gson().toJson(config, ModConfigModel.class);
+		return json.getBytes(StandardCharsets.UTF_8);
+	}
 
-    @Override
-    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
-        if (command.getName().equals(COMMAND)) {
-            if (args.length > 0 && RELOAD.startsWith(args[0])) {
-                return List.of(RELOAD);
-            }
-        }
+	@Override
+	public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+		if (command.getName().equals(COMMAND)) {
+			if (args.length > 0 && RELOAD.startsWith(args[0])) {
+				return List.of(RELOAD);
+			}
+		}
 
-        return super.onTabComplete(sender, command, alias, args);
-    }
+		return super.onTabComplete(sender, command, alias, args);
+	}
 
-    @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (command.getName().equals(COMMAND)) {
-            if (args.length > 0 && args[0].equals(RELOAD)) {
-                if (!sender.hasPermission("xeed.mc.streamotes") && !sender.isOp()) {
-                    sender.sendMessage("Insufficient permissions to execute command!");
-                    return true;
-                }
+	@Override
+	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+		if (command.getName().equals(COMMAND)) {
+			if (args.length > 0 && args[0].equals(RELOAD)) {
+				if (!sender.hasPermission("xeed.mc.streamotes") && !sender.isOp()) {
+					sender.sendMessage("Insufficient permissions to execute command!");
+					return true;
+				}
 
-                reloadModConfig();
-                getServer().sendPluginMessage(this, CHANNEL, createConfigPacket());
-                return true;
-            }
-        }
+				reloadModConfig();
+				getServer().sendPluginMessage(this, CHANNEL, createConfigPacket());
+				return true;
+			}
+		}
 
-        return super.onCommand(sender, command, label, args);
-    }
+		return super.onCommand(sender, command, label, args);
+	}
 }
