98baed1a4a4f153e7854f785712b0ce39d02174e
Xspeed <xspeedpl@windowslive.com>
2024-09-30 21:36:22 +0200

    Secondary lookup method for global Twitch emotes

--- fabricbase/src/main/java/xeed/mc/streamotes/addon/pack/TwitchGlobalPack.java
+++ fabricbase/src/main/java/xeed/mc/streamotes/addon/pack/TwitchGlobalPack.java
@@ -6,7 +6,7 @@ import xeed.mc.streamotes.emoticon.Emoticon;
 import xeed.mc.streamotes.emoticon.EmoticonRegistry;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -15,40 +15,80 @@ public class TwitchGlobalPack {
 	private static final String EMOTE_URL_TEMPLATE = "https://static-cdn.jtvnw.net/emoticons/v2/{{id}}/default/dark/2.0";
 	private static final int PRIO = 1;
 
-	public static void loadMetadata() {
-		try {
-			var apiURL = TwitchEmotesAPI.getURL("https://www.twitchmetrics.net/emotes/");
+	private static void loadSource1() throws IOException {
+		var apiURL = TwitchEmotesAPI.getURL("https://twitchemotes.com/");
 
-			try (var reader = new BufferedReader(new InputStreamReader(apiURL.openStream()))) {
-				String line;
-				while ((line = reader.readLine()) != null) {
-					String prefix = "<a href=\"/e/";
-					int ixStart = line.indexOf(prefix);
-					if (ixStart == -1) continue;
+		try (var reader = new BufferedReader(new InputStreamReader(apiURL.openStream()))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String prefix = "data-regex=\"";
+				int ixStart = line.indexOf(prefix);
+				if (ixStart == -1) continue;
 
-					int ixEnd = line.indexOf("\"", ixStart + prefix.length());
-					if (ixEnd == -1) continue;
+				int ixEnd = line.indexOf("\"", ixStart + prefix.length());
+				if (ixEnd == -1) continue;
 
-					String idCode = line.substring(ixStart + prefix.length(), ixEnd);
+				String code = line.substring(ixStart + prefix.length(), ixEnd);
 
-					ixStart = idCode.indexOf('-');
-					if (ixStart == -1) continue;
+				prefix = "data-image-id=\"";
+				ixStart = line.indexOf(prefix);
+				if (ixStart == -1) continue;
 
-					String id = idCode.substring(0, ixStart);
-					String code = idCode.substring(ixStart + 1);
+				ixEnd = line.indexOf("\"", ixStart + prefix.length());
+				if (ixEnd == -1) continue;
 
-					var emoticon = EmoticonRegistry.registerEmoticon(".Twitch", code, PRIO, TwitchGlobalPack::loadEmoticonImage);
-					if (emoticon != null) {
-						emoticon.setLoadData(id);
-						emoticon.setTooltip("Twitch");
-					}
+				String id = line.substring(ixStart + prefix.length(), ixEnd);
+
+				var emoticon = EmoticonRegistry.registerEmoticon(".Twitch", code, PRIO, TwitchGlobalPack::loadEmoticonImage);
+				if (emoticon != null) {
+					emoticon.setLoadData(id);
+					emoticon.setTooltip("Twitch");
 				}
 			}
 		}
-		catch (FileNotFoundException ignored) {
+	}
+
+	private static void loadSource2() throws IOException {
+		var apiURL = TwitchEmotesAPI.getURL("https://www.twitchmetrics.net/emotes/");
+
+		try (var reader = new BufferedReader(new InputStreamReader(apiURL.openStream()))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String prefix = "<a href=\"/e/";
+				int ixStart = line.indexOf(prefix);
+				if (ixStart == -1) continue;
+
+				int ixEnd = line.indexOf("\"", ixStart + prefix.length());
+				if (ixEnd == -1) continue;
+
+				String idCode = line.substring(ixStart + prefix.length(), ixEnd);
+
+				ixStart = idCode.indexOf('-');
+				if (ixStart == -1) continue;
+
+				String id = idCode.substring(0, ixStart);
+				String code = idCode.substring(ixStart + 1);
+
+				var emoticon = EmoticonRegistry.registerEmoticon(".Twitch", code, PRIO, TwitchGlobalPack::loadEmoticonImage);
+				if (emoticon != null) {
+					emoticon.setLoadData(id);
+					emoticon.setTooltip("Twitch");
+				}
+			}
 		}
-		catch (Exception e) {
-			throw new EmoteLoaderException("Unhandled exception", e);
+	}
+
+	public static void loadMetadata() {
+		try {
+			loadSource1();
+		}
+		catch (IOException ignored) {
+			try {
+				loadSource2();
+			}
+			catch (Exception e) {
+				throw new EmoteLoaderException("Unhandled exception", e);
+			}
 		}
 	}
 
