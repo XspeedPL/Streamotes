c2bc772491eaf0ce6439bc0201343cefb343888c
Xspeed <xspeedpl@windowslive.com>
2024-10-30 17:39:28 +0100

    Tab suggestions partial emote name matching

--- fabricbase/src/main/java/xeed/mc/streamotes/mixin/MixinCommandSuggestor.java
+++ fabricbase/src/main/java/xeed/mc/streamotes/mixin/MixinCommandSuggestor.java
@@ -2,27 +2,28 @@ package xeed.mc.streamotes.mixin;
 
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
-
 import net.minecraft.client.gui.screen.ChatInputSuggestor;
 import net.minecraft.command.CommandSource;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.ArrayList;
-import java.util.concurrent.CompletableFuture;
-
 import xeed.mc.streamotes.emoticon.EmoticonRegistry;
+
+import java.util.Locale;
+import java.util.concurrent.CompletableFuture;
 
 @Mixin(ChatInputSuggestor.class)
 public abstract class MixinCommandSuggestor {
 	@SuppressWarnings("unused")
 	@Redirect(method = "refresh", at = @At(value = "INVOKE", target = "Lnet/minecraft/command/CommandSource;suggestMatching(Ljava/lang/Iterable;Lcom/mojang/brigadier/suggestion/SuggestionsBuilder;)Ljava/util/concurrent/CompletableFuture;"))
 	private CompletableFuture<Suggestions> atRefreshSuggestMatching(Iterable<String> iterable, SuggestionsBuilder builder) {
-		var list = new ArrayList<String>();
-		iterable.forEach(list::add);
-		list.addAll(EmoticonRegistry.getEmoteNames());
-		return CommandSource.suggestMatching(list, builder);
+		String text = builder.getRemaining().toLowerCase(Locale.ROOT);
+
+		for (var emote : EmoticonRegistry.getEmoteNames()) {
+			if (emote.toLowerCase(Locale.ROOT).contains(text))
+				builder.suggest(emote);
+		}
+
+		return CommandSource.suggestMatching(iterable, builder);
 	}
 }
