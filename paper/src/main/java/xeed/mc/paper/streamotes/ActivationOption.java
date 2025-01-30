6e6950b5fe1192be2c1ab4c4840f86013705648b
Xspeed <xspeedpl@windowslive.com>
2025-01-30 12:51:13 +0100

    Include activation option in config

---
+++ paper/src/main/java/xeed/mc/paper/streamotes/ActivationOption.java
@@ -0,0 +1,8 @@
+package xeed.mc.paper.streamotes;
+
+@SuppressWarnings("unused")
+public enum ActivationOption {
+	Required,
+	Optional,
+	Disabled
+}
--- paper/src/main/java/xeed/mc/paper/streamotes/ModConfigModel.java
+++ paper/src/main/java/xeed/mc/paper/streamotes/ModConfigModel.java
@@ -6,34 +6,36 @@ import java.util.List;
 
 @SuppressWarnings("unused")
 public class ModConfigModel {
-    public static void verifyChannels(ModConfigModel model) {
-        var list = new ArrayList<>(model.emoteChannels);
-        int size = list.size();
-        var set = new HashSet<String>(size);
+	public static void verifyChannels(ModConfigModel model) {
+		var list = new ArrayList<>(model.emoteChannels);
+		int size = list.size();
+		var set = new HashSet<String>(size);
 
-        for (int i = list.size() - 1; i >= 0; --i) {
-            var item = list.get(i);
-            if (item == null || item.length() < 4 || item.length() > 25 || !Streamotes.VALID_CHANNEL_PATTERN.matcher(item).find() || !set.add(item)) {
-                list.remove(i);
-            }
-        }
+		for (int i = list.size() - 1; i >= 0; --i) {
+			var item = list.get(i);
+			if (item == null || item.length() < 4 || item.length() > 25 || !Streamotes.VALID_CHANNEL_PATTERN.matcher(item).find() || !set.add(item)) {
+				list.remove(i);
+			}
+		}
 
-        if (size != list.size()) {
-            model.emoteChannels = list;
-        }
-    }
+		if (size != list.size()) {
+			model.emoteChannels = list;
+		}
+	}
 
-    public List<String> emoteChannels = List.of("Spookie_Rose", "fifigoesree", "Mifuyu");
+	public List<String> emoteChannels = List.of("Spookie_Rose", "fifigoesree", "Mifuyu");
 
-    public boolean twitchGlobalEmotes = true;
-    public boolean twitchSubscriberEmotes = true;
+	public boolean twitchGlobalEmotes = true;
+	public boolean twitchSubscriberEmotes = true;
 
-    public boolean bttvEmotes = true;
-    public boolean bttvChannelEmotes = true;
+	public boolean bttvEmotes = true;
+	public boolean bttvChannelEmotes = true;
 
-    public boolean ffzEmotes = true;
-    public boolean ffzChannelEmotes = true;
+	public boolean ffzEmotes = true;
+	public boolean ffzChannelEmotes = true;
 
-    public boolean x7tvEmotes = true;
-    public boolean x7tvChannelEmotes = true;
+	public boolean x7tvEmotes = true;
+	public boolean x7tvChannelEmotes = true;
+
+	public ActivationOption activationMode = ActivationOption.Optional;
 }
