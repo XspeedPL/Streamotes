9b966ed39531dd2ac5ec161e4f32e04f0a8db913
Xspeed <xspeedpl@windowslive.com>
2025-05-04 23:46:27 +0200

    Renamed fabric121 to fabric1210

---
+++ fabric1210/src/main/java/xeed/mc/streamotes/Compat.java
@@ -0,0 +1,77 @@
+package xeed.mc.streamotes;
+
+import com.google.common.util.concurrent.Runnables;
+import com.mojang.blaze3d.platform.TextureUtil;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.render.*;
+import net.minecraft.client.texture.NativeImage;
+import net.minecraft.client.toast.SystemToast;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.Style;
+import net.minecraft.util.Util;
+import xeed.mc.streamotes.emoticon.Emoticon;
+import xeed.mc.streamotes.emoticon.EmoticonRegistry;
+
+import java.util.function.Function;
+
+public class Compat {
+	private static final RenderPhase.ShaderProgram PROGRAM = new RenderPhase.ShaderProgram(GameRenderer::getPositionTexColorProgram);
+
+	public static final Function<Emoticon, RenderLayer> LAYER = Util.memoize(icon ->
+		RenderLayer.of("emote-" + icon.code, VertexFormats.POSITION_TEXTURE_COLOR, VertexFormat.DrawMode.QUADS, 2048, false, false,
+			RenderLayer.MultiPhaseParameters.builder().texture(new RenderPhase.TextureBase(icon.getTexture()::onApply, Runnables.doNothing()))
+				.program(PROGRAM).transparency(RenderPhase.Transparency.TRANSLUCENT_TRANSPARENCY).build(false)));
+
+	public static void onInitializeClient(Streamotes.StringAction handler) {
+		ClientPlayNetworking.registerGlobalReceiver(JsonPayload.PACKET_ID, (packet, context) -> {
+			handler.apply(packet.json());
+		});
+	}
+
+	public static SystemToast.Type makeToastType() {
+		return new SystemToast.Type(4000);
+	}
+
+	public static Style makeEmoteStyle(Emoticon icon) {
+		return Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, icon.getName()))
+			.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, icon.getTooltip()));
+	}
+
+	public static Emoticon getEmote(Style style) {
+		var ev = style.getClickEvent();
+		return ev != null && ev.getAction() == ClickEvent.Action.COPY_TO_CLIPBOARD
+			? EmoticonRegistry.fromName(ev.getValue())
+			: null;
+	}
+
+	public static void nextVertex(VertexConsumer builder) {
+	}
+
+	public static class Texture implements AutoCloseable {
+		private int glId = -1;
+
+		public boolean isLoaded() {
+			return glId != -1;
+		}
+
+		public void onApply() {
+			RenderSystem.setShaderTexture(0, glId);
+		}
+
+		public void upload(String label, NativeImage buffer) {
+			if (glId == -1) glId = TextureUtil.generateTextureId();
+			TextureUtil.prepareImage(glId, 0, buffer.getWidth(), buffer.getHeight());
+
+			buffer.upload(0, 0, 0, 0, 0, buffer.getWidth(), buffer.getHeight(), false, false, true, true);
+		}
+
+		public void close() {
+			if (glId != -1) {
+				TextureUtil.releaseTextureId(glId);
+				glId = -1;
+			}
+		}
+	}
+}
