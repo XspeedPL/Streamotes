e9500f2866dddab55f0b81de516428dba8eddfa6
Xspeed <xspeedpl@windowslive.com>
2025-07-01 03:03:17 +0200

    Support for Minecraft 1.21.6

---
+++ fabric1216/src/main/java/xeed/mc/streamotes/Compat.java
@@ -0,0 +1,94 @@
+package xeed.mc.streamotes;
+
+import com.google.common.util.concurrent.Runnables;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.textures.FilterMode;
+import com.mojang.blaze3d.textures.GpuTexture;
+import com.mojang.blaze3d.textures.GpuTextureView;
+import com.mojang.blaze3d.textures.TextureFormat;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.gl.RenderPipelines;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.RenderPhase;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.texture.NativeImage;
+import net.minecraft.client.toast.SystemToast;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.Style;
+import xeed.mc.streamotes.emoticon.Emoticon;
+import xeed.mc.streamotes.emoticon.EmoticonRegistry;
+
+public class Compat {
+	public static RenderLayer layerFunc(Emoticon icon) {
+		return RenderLayer.of("emote-" + icon.getName(), 2048, false, false, RenderPipelines.GUI_TEXTURED,
+			RenderLayer.MultiPhaseParameters.builder().texture(new RenderPhase.TextureBase(icon.getTexture()::onApply, Runnables.doNothing()))
+				.build(false));
+	}
+
+	public static void onInitializeClient(Streamotes.StringAction handler) {
+		ClientPlayNetworking.registerGlobalReceiver(JsonPayload.PACKET_ID, (packet, context) -> {
+			handler.apply(packet.json());
+		});
+	}
+
+	public static SystemToast.Type makeToastType() {
+		return new SystemToast.Type(4000);
+	}
+
+	public static Style makeEmoteStyle(Emoticon icon) {
+		return Style.EMPTY.withClickEvent(new ClickEvent.CopyToClipboard(icon.getName()))
+			.withHoverEvent(new HoverEvent.ShowText(icon.getTooltip()));
+	}
+
+	public static Emoticon getEmote(Style style) {
+		return style.getClickEvent() instanceof ClickEvent.CopyToClipboard(String value)
+			? EmoticonRegistry.fromName(value)
+			: null;
+	}
+
+	public static void nextVertex(VertexConsumer consumer) {
+	}
+
+	public static class Texture implements AutoCloseable {
+		private GpuTexture texture;
+		private GpuTextureView view;
+
+		public boolean isLoaded() {
+			return view != null && !view.isClosed();
+		}
+
+		public GpuTextureView getView() {
+			return view;
+		}
+
+		public void onApply() {
+			if (texture != null) RenderSystem.setShaderTexture(0, view);
+		}
+
+		public void upload(String label, NativeImage buffer) {
+			var dev = RenderSystem.getDevice();
+
+			if (texture == null) {
+				texture = dev.createTexture(label, GpuTexture.USAGE_COPY_DST | GpuTexture.USAGE_TEXTURE_BINDING, TextureFormat.RGBA8, buffer.getWidth(), buffer.getHeight(), 1, 1);
+				texture.setTextureFilter(FilterMode.LINEAR, FilterMode.NEAREST, true);
+			}
+			if (view == null) {
+				view = dev.createTextureView(texture);
+			}
+
+			RenderSystem.getDevice().createCommandEncoder().writeToTexture(texture, buffer);
+		}
+
+		public void close() {
+			if (view != null && !view.isClosed()) {
+				view.close();
+				view = null;
+			}
+			if (texture != null && !texture.isClosed()) {
+				texture.close();
+				texture = null;
+			}
+		}
+	}
+}
---
+++ fabric1216/src/main/java/xeed/mc/streamotes/EmoticonGlyph.java
@@ -0,0 +1,65 @@
+package xeed.mc.streamotes;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.BakedGlyph;
+import net.minecraft.client.font.TextRenderLayerSet;
+import net.minecraft.client.gl.RenderPipelines;
+import net.minecraft.client.render.VertexConsumer;
+import org.joml.Matrix4f;
+import xeed.mc.streamotes.emoticon.Emoticon;
+
+public class EmoticonGlyph extends BakedGlyph {
+	private final Emoticon icon;
+	private final float x;
+	private final float y;
+	private final float w;
+	private final float h;
+	private final int color;
+
+	public static EmoticonGlyph of(Emoticon icon, float x, float y, int color) {
+		var layer = DrawerCommons.getLayer(icon);
+		var layerSet = new TextRenderLayerSet(layer, layer, layer, RenderPipelines.GUI_TEXTURED);
+
+		var client = MinecraftClient.getInstance();
+		float lineSpacing = (float)(client.options.getChatLineSpacing().getValue() * 4);
+		float height = client.textRenderer.fontHeight + lineSpacing * 2;
+
+		return new EmoticonGlyph(layerSet, icon, x, y - lineSpacing - 1f, icon.getRenderWidth(height), height, color);
+	}
+
+	public EmoticonGlyph(TextRenderLayerSet layerSet, Emoticon icon, float x, float y, float w, float h, int color) {
+		super(layerSet, icon.getTexture().getView(), 0f, 1f, 0f, 1f, x, x + w, y, y + h);
+		this.icon = icon;
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.color = color;
+	}
+
+	@Override
+	public void draw(BakedGlyph.DrawnGlyph glyph, Matrix4f matrix, VertexConsumer consumer, int light, boolean fixedZ) {
+		if (icon.isAnimated()) icon.updateAnimation();
+
+		drawQuad(consumer, matrix, x, y, 0f, w, h,
+			icon.getCurrentFrameTexCoordX(), icon.getCurrentFrameTexCoordY(),
+			icon.getWidth(), icon.getHeight(), icon.getSheetWidth(), icon.getSheetHeight(),
+			color);
+	}
+
+	private static void drawQuad(VertexConsumer consumer, Matrix4f matrix, float x0, float y0, float z, float w, float h, float u, float v, float regionW, float regionH, int texW, int texH, int color) {
+		final float x1 = x0 + w;
+		final float y1 = y0 + h;
+
+		final float u0 = u / texW;
+		final float u1 = (u + regionW) / texW;
+
+		final float v0 = v / texH;
+		final float v1 = (v + regionH) / texH;
+
+		consumer.vertex(matrix, x0, y1, z).texture(u0, v1).color(color);
+		consumer.vertex(matrix, x1, y1, z).texture(u1, v1).color(color);
+		consumer.vertex(matrix, x1, y0, z).texture(u1, v0).color(color);
+		consumer.vertex(matrix, x0, y0, z).texture(u0, v0).color(color);
+	}
+}
