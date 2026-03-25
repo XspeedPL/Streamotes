952f162747b4d53211dbd10bcadd642dfb437b01
Xspeed <xspeedpl@windowslive.com>
2026-03-25 06:25:53 +0100

    Support for Minecraft 26.1

---
+++ fabric26_1/src/main/java/xeed/mc/streamotes/CompatServer.java
@@ -0,0 +1,31 @@
+package xeed.mc.streamotes;
+
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.resources.Identifier;
+import net.minecraft.server.permissions.Permissions;
+
+import java.util.Objects;
+
+public class CompatServer {
+	public static final Identifier IDENT = Objects.requireNonNull(Identifier.tryBuild("xeed", StreamotesCommon.NAME));
+
+	public static boolean permissionPredicate(CommandSourceStack source) {
+		return source.permissions().hasPermission(Permissions.COMMANDS_ADMIN);
+	}
+
+	public static void onInitializeServer() {
+		PayloadTypeRegistry.clientboundPlay().register(JsonPayload.PACKET_ID, JsonPayload.PACKET_CODEC);
+	}
+
+	public static Packet<?> createConfigPacket(String json) {
+		return ServerPlayNetworking.createClientboundPacket(new JsonPayload(json));
+	}
+
+	public static void sendFeedback(CommandSourceStack source, Component message, boolean broadcastToOps) {
+		source.sendSuccess(() -> message, broadcastToOps);
+	}
+}
